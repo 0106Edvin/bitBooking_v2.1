b6f295e4fc628f79c8c6afce39308204f8330a56
Edvin Mulabdic <edvin.mulabdic@bitcamp.ba>
2015-09-21 01:13:47 +0200

    Feature, Hotel

---
+++ app/controllers/Features.java
@@ -0,0 +1,53 @@
+package controllers;
+
+import com.avaje.ebean.Ebean;
+import models.Feature;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.feature.createFeature;
+import views.html.feature.editFeature;
+
+/**
+ * Created by ajla.eltabari on 09/09/15.
+ */
+public class Features extends Controller {
+
+    private Form<Feature> featureForm = Form.form(Feature.class);
+
+    public Result saveFeature() {
+        Form<Feature> boundForm = featureForm.bindFromRequest();
+
+        Feature feature = boundForm.get();
+        feature.save();
+        return redirect(routes.Application.index());
+    }
+//    public Result deleteFeature(Integer id){
+//        Feature feature = Feature.findFeatureById(id);
+//        Ebean.delete(feature);
+//
+//        return ok(routes.Application.index());
+//    }
+//    public Result editfeature(Integer id){
+//        Feature feature = Feature.findFeatureById(id);
+//        return ok(editFeature.render(feature));
+//    }
+//
+//    public Result updateFeature(Integer id){
+//        Form<Feature> boundForm = featureForm.bindFromRequest();
+//        Feature feature = Feature.findFeatureById(id);
+//
+//        String name = boundForm.bindFromRequest().field("name").value();
+//
+//        feature.name = name;
+//
+//        Ebean.update(feature);
+//
+//        return ok(routes.Application.index());
+//    }
+
+    public Result createFeature(){
+        return ok(createFeature.render());
+    }
+
+}
---
+++ app/models/Hotel.java
@@ -0,0 +1,70 @@
+package models;
+
+import com.avaje.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.List;
+
+/**
+ * Created by Edvin Mulabdic on 9/6/2015.
+ */
+
+@Entity
+public class Hotel extends Model {
+    public static Finder<String, Hotel> finder = new Finder<>(Hotel.class);
+
+    @Id
+    public Integer id;
+    public String name;
+    public String location;
+    public String description;
+//    public String coordinateX;
+//    public String coordinateY;
+//    public Integer sellerId;
+
+    @ManyToMany
+    public List<Feature> features;
+//
+//    @OneToMany(cascade = CascadeType.ALL)
+//    public List<Image> images;
+//
+//    @OneToMany
+//    public List<Room> rooms;
+//
+//    @OneToMany
+//    public List<Comment> comments;
+
+    public Hotel(Integer id, String name, String location, String description, List<Feature> features){//, String coordinateX, String coordinateY,
+               //  Integer sellerId}, List<Image> images, List<Room> rooms, List<Comment> comments) {
+
+
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.description = description;
+//        this.coordinateX = coordinateX;
+//        this.coordinateY = coordinateY;
+//        this.sellerId = sellerId;
+//        this.features = features;
+//        this.images = images;
+        this.features = features;
+//        this.rooms = rooms;
+//        this.comments = comments;
+
+
+    }
+    //method that finds hotel by id
+    public static Hotel findHotelById(Integer id) {
+        Hotel hotel = finder.where().eq("id", id).findUnique();
+
+        return hotel;
+    }
+
+    @Override
+    public String toString() {
+        return (id.toString() + " " + name + " " + location );
+    }
+
+}
