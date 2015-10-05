c36a69da1e200d0dfd696f845e7a626a81a2136a
Ajla El Tabari <ajla.eltabari@bitcamp.ba>
2015-10-05 23:57:11 +0200

    Restaurant model and minor bug fixes

--- app/models/Restaurant.java
+++ app/models/Restaurant.java
@@ -2,16 +2,17 @@ package models;
 
 import com.avaje.ebean.Model;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
+import java.util.Date;
 import java.util.List;
 
 /**
  * Created by ajla.eltabari on 05/10/15.
  */
+@Entity
 public class Restaurant extends Model {
+    public static Finder<String, Restaurant> finder = new Finder<>(Restaurant.class);
+
     @Id
     public Integer id;
     public String name;
@@ -20,9 +21,28 @@ public class Restaurant extends Model {
     @Column(columnDefinition = "TEXT")
     public String description;
 
+    @OneToOne
+    public Hotel hotel;
+
     @OneToMany(cascade = CascadeType.ALL)
     public List<Image> images;
 
+    public String timestamp;
 
+    public Restaurant(Integer id, String name, String type, String description, Hotel hotel, List<Image> images) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.description = description;
+        this.hotel = hotel;
+        this.images = images;
+        this.timestamp = new Date().toString();
+    }
 
+    //method that finds restaurant by hotel_id
+    public static Restaurant findRestaurantByHotelId(Integer hotelId) {
+        Restaurant restaurant = finder.where().eq("hotel_id", hotelId).findUnique();
+
+        return restaurant;
+    }
 }
