b8e71ea4eb6c25dc4a5d3a94a9fcab08bb46118b
boristomic <boris.tomic@bitcamp.ba>
2015-10-10 18:30:35 +0200

    fixed possible NPE

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,21 +16,25 @@ public class Application extends Controller {
 
     public Result index() {
         String ipAddress = request().remoteAddress();
+        Logger.debug(ipAddress);
         SiteStats tempStat = SiteStats.finder.where().eq("ip_address", ipAddress).findUnique();
         AppUser temp = SessionsAndCookies.getCurrentUser(ctx());
         SiteStats stats = new SiteStats();
         stats.ipAddress = ipAddress;
         if (temp != null) {
             stats.setCreatedBy(temp.firstname, temp.lastname);
-            tempStat.setUpdatedBy(temp.firstname, temp.lastname);
+            if (tempStat != null) {
+                tempStat.setUpdatedBy(temp.firstname, temp.lastname);
+            }
         } else {
             stats.createdBy = "Anonimous user";
-            tempStat.updatedBy = "Anonimous user";
+            if (tempStat != null) {
+                tempStat.updatedBy = "Anonimous user";
+            }
         }
         try {
             stats.save();
         } catch (PersistenceException e) {
-
             tempStat.update();
         }
         List<Hotel> hotels = Hotel.finder.all();
