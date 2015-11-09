5bce980a106ce9a4d7da164b5c10899580eb2aab
boristomic <boris.tomic@bitcamp.ba>
2015-11-09 11:51:43 +0100

    completed faq refactoring

--- app/controllers/Questions.java
+++ app/controllers/Questions.java
@@ -2,6 +2,7 @@ package controllers;
 
 import com.avaje.ebean.Model;
 import helpers.Authenticators;
+import helpers.CommonHelperMethods;
 import models.Feature;
 import models.Question;
 import play.Logger;
@@ -22,72 +23,102 @@ import java.util.List;
  */
 public class Questions extends Controller {
 
-    private Form<Question> questionForm = Form.form(Question.class);
-    List<Question> questions = Question.finder.all();
-
-
-
+    /**
+     * Renders view for admin so he/she can create new FAQ
+     *
+     * @return
+     */
     @Security.Authenticated(Authenticators.AdminFilter.class)
-    public Result createFAQ(){
+    public Result createFAQ() {
         return ok(createFAQ.render());
     }
 
+    /**
+     * Renders a view for admin so he/she can edit specific FAQ
+     *
+     * @param Id <code>Integer</code> type value of question id
+     * @return
+     */
     @Security.Authenticated(Authenticators.AdminFilter.class)
-    public Result editFAQ(Integer Id){
+    public Result editFAQ(Integer Id) {
         Question questions = Question.findQuestionById(Id);
 
         return ok(editFAQ.render(questions));
     }
 
-    //This method saves the FAQ
+    /**
+     * Saves new FAW to database, checks if input fields are valid, then tries to save to database.
+     *
+     * @return
+     */
     @Security.Authenticated(Authenticators.AdminFilter.class)
-    public Result saveFAQ(){
+    public Result saveFAQ() {
         DynamicForm form = Form.form().bindFromRequest();
-        Question q = new Question();
-        q.question = form.field("question").value();
-        q.answer = form.field("answer").value();
-        q.save();
-
-        flash("save", "A new FAQ was created!");
-
+        String question = form.field("question").value();
+        String answer = form.field("answer").value();
+        if (!CommonHelperMethods.validateInputString(question) || !CommonHelperMethods.validateInputString(answer)) {
+            flash("error-search", "Seems like you didn't write anything in input fields.");
+            return redirect(routes.Questions.seeAll());
+        }
+        if (Question.createNewFAQ(question, answer)) {
+            flash("save", "A new FAQ was created!");
+            return redirect(routes.Questions.seeAll());
+        }
+        flash("error-search", "Failed to save FAQ.");
         return redirect(routes.Questions.seeAll());
     }
 
 
-    //This method updates the FAQ
+    /**
+     * Finds specific FAQ and tries to update it with new values.
+     *
+     * @param Id <code>Integer</code> type value of FAQ id
+     * @return
+     */
     @Security.Authenticated(Authenticators.AdminFilter.class)
     public Result updateFAQ(Integer Id) {
-
-        Question q = Question.findQuestionById(Id);
         DynamicForm form = Form.form().bindFromRequest();
-        q.question = form.field("question").value();
-        q.answer = form.field("answer").value();
+        String question = form.field("question").value();
+        String answer = form.field("answer").value();
 
+        if (!CommonHelperMethods.validateInputString(question) || !CommonHelperMethods.validateInputString(answer)) {
+            flash("error-search", "Seems like you didn't write anything in input fields.");
+            return redirect(routes.Questions.seeAll());
+        }
 
-        q.update();
-        flash("update", "The FAQ was updated!");
+        if (Question.updateFAQ(Id, question, answer)) {
+            flash("update", "The FAQ was updated!");
+            return redirect(routes.Questions.seeAll());
+        }
+        flash("error-search", "The FAQ could not be updated!");
         return redirect(routes.Questions.seeAll());
     }
 
-
-    //This method deletes the FAQ
+    /**
+     * Tries to delete specific FAQ from database.
+     *
+     * @param Id <code>Integer</code> type value of FAQ id
+     * @return
+     */
     @Security.Authenticated(Authenticators.AdminFilter.class)
     public Result deleteFAQ(Integer Id) {
-        Question q = Question.findQuestionById(Id);
-
-        q.delete();
-        flash("delete", "The FAQ was deleted!");
+        if (Question.deleteFAQ(Id)) {
+            flash("delete", "The FAQ was deleted!");
+            return redirect(routes.Questions.seeAll());
+        }
+        flash("error-search", "The FAQ could not be deleted!");
         return redirect(routes.Questions.seeAll());
     }
 
-
-    //shows all the FAQ's
-    public Result seeAll(){
-        List<Question> questions = Question.finder.all();
-
+    /**
+     * Renders all FAQ for public users to see
+     *
+     * @return
+     */
+    public Result seeAll() {
+        List<Question> questions = Question.getAllQuestions();
         return ok(showFAQ.render(questions));
     }
-
 
 
 }
